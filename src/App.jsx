b6b300d5c5129f7942d1911efa89989989fbbfc2
alirezaghnzd <changlee76301@gmail.com>
2025-07-13 16:09:38 +0330

    add router in project and setting up

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import CreateOrder from "./features/order/CreateOrder";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "menu", element: <Menu /> },
+  { path: "order/new", element: <CreateOrder /> },
+  { path: "order/:orderId", element: <Order /> },
+  { path: "cart", element: <Cart /> },
+]);
 export default function App() {
-  return <div>hello friend</div>;
+  return <RouterProvider router={router} />;
 }
